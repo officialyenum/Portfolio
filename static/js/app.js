f9cd2885b23e752d8c2ec0a3d5e40a745220217f
Opone Chukwuyenum <40031850+officialyenum@users.noreply.github.com>
2019-01-18 23:15:31 +0100

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,12 @@
 /* add two numbers and get remainder */
 
+//
 function getModulo() {
     var x = document.getElementById("num1").value;
     var y = document.getElementById("num2").value;
     // If x is Not a number
-    if (isNaN(x) || isNaN(y) ) {
-        text = "One or more Input is not a number";
+    if (isNaN(x) || isNaN(y)) {
+        text = "One or more Input is not number";
     } else {
         if (x == "" || y == "") {
             text = "One or more Input is empty";
